1f43c96c35140977e437b531c51a9bd7db9bcd25
Claire Martin <cmmartin@ualberta.ca>
2022-02-26 15:32:45 -0700

    updated permanent beams and added save event locations

--- VerticalSlice/js/plugins.js
+++ VerticalSlice/js/plugins.js
@@ -5,5 +5,6 @@ var $plugins =
 {"name":"lineofsight","status":true,"description":"","parameters":{}},
 {"name":"DMY_SkipVideo","status":true,"description":"Skip video on key press","parameters":{"Disabler switch":"0"}},
 {"name":"Galv_VariableBar","status":true,"description":"(v.1.0) A graphical bar that displays in the map scene to show the current value of a max value visually using variables.","parameters":{}},
-{"name":"lineofsight2","status":true,"description":"","parameters":{}}
+{"name":"lineofsight2","status":true,"description":"","parameters":{}},
+{"name":"YEP_SaveEventLocations","status":true,"description":"v1.06 Enable specified maps to memorize the locations of\nevents when leaving and loading them upon reentering map.","parameters":{}}
 ];
